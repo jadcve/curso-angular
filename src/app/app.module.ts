446f996bab10bd6a4d79de3722b6b7b91bb9b1a2
José Alaín Díaz Carrero <jadcve@gmail.com>
2024-04-18 23:26:51 -0400

    Reinstalando de nuevo cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common'; // Add this line
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
